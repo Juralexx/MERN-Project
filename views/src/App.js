706bc7469c7babd4a0fdec893bc6b82808ea5348
Juralexx <alexandre.vurbier@gmail.com>
2021-12-17 01:15:12 +0100

    Version 0.1

--- views/src/App.js
+++ views/src/App.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Index from './components/Routes/index';
 import { UidContext } from "./components/AppContext"
+import axios from 'axios';
 
 function App() {
   const [uid, setUid] = useState(null)
+
+  useEffect(() => {
+    const fetchToken = async() => {
+    await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials: true,
+    })
+      .then((res) => { setUid(res.data) } )
+      .catch((err) => console.log('No token'))
+    }
+  fetchToken();
+  }, [uid]);
+
+
   return (
     <UidContext.Provider value={uid}>
       <Index />
