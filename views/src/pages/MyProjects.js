d6166922880447fb88efd2ce4365eee53a5846c7
Juralexx <alexandre.vurbier@gmail.com>
2022-01-18 23:31:17 +0100

    Version 0.1

--- views/src/pages/MyProjects.js
+++ views/src/pages/MyProjects.js
@@ -7,18 +7,20 @@ import AddBloc from '../components/project/AddBloc';
 const MyProjects = () => {
   const userData = useSelector((state) => state.userReducer)
   const id = userData._id
-  const [project, setProject] = useState({});
+  const [nbOfRes, setNbOfRes] = useState([])
+  const [resBody, setResBody] = useState([])
+  const [axiosRes, setAxiosRes] = useState([])
   localStorage.setItem('id', `${id}`)
 
   document.getElementsByClassName('titleproject').innerHTML = ""
 
   useEffect(() => {
     const userId = localStorage.getItem('id', `${id}`)
-    console.log("USER ID : " + userId)
     const fetch = async () => {
       try {
         const { data } = await axios.get(`${process.env.REACT_APP_API_URL}api/user/${userId}`)
         const checkarray = data.createdProjects
+        setNbOfRes(checkarray.length)
 
         if (checkarray.length > 0) {
           const projects = checkarray.map(async (projectID) => {
@@ -28,8 +30,9 @@ const MyProjects = () => {
               .catch((e) => console.error(e));
           })
           Promise.all(projects).then((res) => {
+            setAxiosRes(res)
             res.map((project) => {
-              document.querySelector('.titleproject').innerHTML = project.title
+              setResBody(project)
             })
           });
         }
@@ -45,19 +48,30 @@ const MyProjects = () => {
       <div className="projects-container">
         <AddBloc />
 
-        <>
-          <div className="container">
-            <h1 className="titleproject"></h1>
-            {/* <p>Catégorie : {project.category}</p>
-            <p>Posté par : {project.posterPseudo}</p>
-            <p>Date de création : {dateParser(project.createdAt)}</p>
-            <p>Dernière modification : {dateParser(project.updatedAt)}</p>
-            <p>Localisation : {project.location}</p>
-            <p>Date de fin potentielle : {dateParser(project.end)}</p>
-            <p>Nombre de personne : {project.numberofcontributors}</p>
-            <p>Description : {project.content}</p> */}
-          </div>
-        </>
+        <div className="container">
+          {(nbOfRes > 0) ? (
+            axiosRes.map((element, key) => {
+              return (
+                <div key={key}>
+                  <h1>ID : {element._id}</h1>
+                  <p>Titre : {element.title}</p>
+                  <p>Catégorie : {element.category}</p>
+                  <p>Posté par : {element.posterPseudo}</p>
+                  <p>Date de création : {dateParser(element.createdAt)}</p>
+                  <p>Dernière modification : {dateParser(element.updatedAt)}</p>
+                  <p>Localisation : {element.location}</p>
+                  <p>Date de fin potentielle : {dateParser(element.end)}</p>
+                  <p>Nombre de personne : {element.numberofcontributors}</p>
+                  <p>Description : {element.content}</p>
+                </div>
+              )
+            })
+          ) : (
+            <div>
+              <p>Vous n'avez pas encore ajouté de projet</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
