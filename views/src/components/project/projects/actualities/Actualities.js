6e18059fc0e3ac59f860c77071daa2247f2d9888
Juralexx <alexandre.vurbier@gmail.com>
2022-04-13 01:25:29 +0200

    Version 0.3

---
+++ views/src/components/project/projects/actualities/Actualities.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import { Button } from '../../../tools/components/Button'
+import { convertDeltaToHTML } from '../../../tools/functions/function'
+import { dateParser } from '../../../Utils'
+import EditActuality from './EditActuality'
+
+const Actualities = ({ project, user }) => {
+
+    return (
+        <Routes>
+            <Route index element={
+                <div className="project-actuality">
+                    <div className="project-actuality-container">
+                        <div className="header flex justify-between mb-5">
+                            <h2>Actualités</h2>
+                        </div>
+                        <div className="actuality-container">
+                            {project.actualities.map((element, key) => {
+                                return (
+                                    <div className="actuality-content" key={key}>
+                                        <div className="actuality-img"><img src={element.pictures[0]} alt={element.title} /></div>
+                                        <div className="actuality-description">
+                                            <h3>{element.title}</h3>
+                                            <div className="date">{dateParser(element.date)}</div>
+                                            <p className="description" dangerouslySetInnerHTML={convertDeltaToHTML(element.description)}></p>
+                                            <div className="btn-container">
+                                                <Link to={`${element.url}/edit`}><Button text="Modifier"></Button></Link>
+                                                <Button text="Voir" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            } />
+
+            <Route path=":url/edit" element={
+                <EditActuality
+                    project={project}
+                />
+            } />
+        </Routes>
+    )
+}
+
+export default Actualities
