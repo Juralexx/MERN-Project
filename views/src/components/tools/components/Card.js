ac052f50dcc11526ad78804a4c70cb9908b84c08
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-01-19 17:44:57 +0100

    Version 0.4

--- views/src/components/tools/components/Card.js
+++ views/src/components/tools/components/Card.js
@@ -1,65 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { dateParser, fullImage } from '../../Utils';
-import { stateToBackground, stateToString } from '../../projects/functions';
+import Icon from '../icons/Icon';
 import FavoriteButton from './FavoriteButton'
 import FollowersButton from './FollowersButton'
+import FollowersModal from './FollowersModal';
 import LikersButton from './LikersButton'
-import Icon from '../icons/Icon';
+import LikersModal from './LikersModal';
+import { dateParser, fullImage } from '../../Utils';
+import { stateToBackground, stateToString } from '../../projects/functions';
 
-const Card = ({ element, setProject, setOpenLikersModal, setOpenFollowersModal, className }) => {
+const Card = ({ project, user, websocket, className }) => {
+    const [openFollowersModal, setOpenFollowersModal] = useState(false)
+    const [openLikersModal, setOpenLikersModal] = useState(false)
+
     return (
-        <div className={className ? 'card ' + className : 'card'}>
-            <div className="card-img" style={fullImage(`${process.env.REACT_APP_API_URL}files/img/paysage-3.jpg`)}></div>
-            <div className={`state ${stateToBackground(element)}`}>
-                {stateToString(element.state)}
-            </div>
-            <FavoriteButton
-                project={element}
-            />
-            <div className="card-body">
-                <Link to={"/project/" + element.URLID + "/" + element.URL}>
-                    <div className="card-title">
-                        <h4>
-                            {element.title}
-                        </h4>
-                        <p>
-                            <Icon name="Position" />
-                            <span>{element.location.city + ", " + element.location.department}</span>
-                        </p>
-                        <p>
-                            <Icon name="List" />
-                            <span>{element.category}</span>
-                        </p>
-                    </div>
-                </Link>
-                <div className="card-description">
-                    {element.description}
+        <>
+            <div className={className ? 'card ' + className : 'card'}>
+                <div className="card-img" style={fullImage(`${process.env.REACT_APP_API_URL}files/img/paysage-3.jpg`)}></div>
+                <div className={`state ${stateToBackground(project)}`}>
+                    {stateToString(project.state)}
                 </div>
-                <div className="card-footer flex col-12">
-                    <div className="footer-left col-8">
-                        <LikersButton project={element} onClick={() => {
-                            setProject(element)
-                            setOpenLikersModal(true)
-                        }} />
-                        <FollowersButton project={element} onClick={() => {
-                            setProject(element)
-                            setOpenFollowersModal(true)
-                        }} />
-                    </div>
-                    <div className="footer-right col-4 flex pt-2">
-                        <div className="footer-name">
-                            <p className="name">
-                                <Link to={`/user/${element.poster.pseudo}`}>
-                                    {element.poster.pseudo}
-                                </Link>
+                {project.poster._id !== user._id &&
+                    <FavoriteButton project={project} />
+                }
+                <div className="card-body">
+                    <Link to={"/project/" + project.URLID + "/" + project.URL}>
+                        <div className="card-title">
+                            <h4>{project.title}</h4>
+                            <p>
+                                <Icon name="Position" />
+                                <span>{project.location.city + ", " + project.location.department}</span>
                             </p>
-                            <p className="date">{dateParser(element.createdAt)}</p>
+                            <p>
+                                <Icon name="List" />
+                                <span>{project.category}</span>
+                            </p>
+                        </div>
+                    </Link>
+                    <div className="card-description">
+                        {project.description}
+                    </div>
+                    <div className="card-footer flex col-12">
+                        <div className="footer-left col-8">
+                            <LikersButton
+                                project={project}
+                                onClick={() => setOpenLikersModal(true)}
+                            />
+                            <FollowersButton
+                                project={project}
+                                onClick={() => setOpenFollowersModal(true)}
+                            />
+                        </div>
+                        <div className="footer-right col-4 flex pt-2">
+                            <div className="footer-name">
+                                <p className="name">
+                                    <Link to={`/user/${project.poster.pseudo}`}>
+                                        {project.poster.pseudo}
+                                    </Link>
+                                </p>
+                                <p className="date">{dateParser(project.createdAt)}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            {openFollowersModal &&
+                <FollowersModal
+                    user={user}
+                    project={project}
+                    websocket={websocket}
+                    open={openFollowersModal}
+                    setOpen={setOpenFollowersModal}
+                />
+            }
+            {openLikersModal &&
+                <LikersModal
+                    user={user}
+                    project={project}
+                    websocket={websocket}
+                    open={openLikersModal}
+                    setOpen={setOpenLikersModal}
+                />
+            }
+        </>
     )
 }
 
